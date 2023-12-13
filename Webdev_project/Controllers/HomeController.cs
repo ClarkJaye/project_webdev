bc34cca8b7223eec71fb0ec7cfbe23cead78cbab
jayclark <jayclarkmonzon@gmail.com>
2023-12-13 15:46:14 +0800

    2nd commit

--- Webdev_project/Controllers/HomeController.cs
+++ Webdev_project/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webdev_project.Models;
 
 namespace Webdev_project.Controllers
 {
@@ -14,19 +15,82 @@ namespace Webdev_project.Controllers
         }
 
 
-        public ActionResult Login()
+        public ActionResult ProfilePage()
         {
-            ViewBag.Message = "Your Login page.";
-
             return View();
         }
 
-        public ActionResult Register()
+        public ActionResult LoginFailed()
         {
-            ViewBag.Message = "Your Register page.";
-
             return View();
         }
+
+
+
+        [HttpPost]
+        public ActionResult AddUserToDatabase(FormCollection fc)
+        {
+            String firstName = fc["firstname"];
+            String lastName = fc["lastname"];
+            String gender = fc["gender"];
+            String email = fc["email"];
+            String address = fc["address"];
+            String password = fc["password"];
+            String confirmPassword = fc["confirm-password"];
+
+            //var redirectPage = "";
+        
+            if (password == confirmPassword)
+            {
+                user_table use = new user_table();
+                use.firstname = firstName;
+                use.lastname = lastName;
+                use.gender = gender;
+                use.email = email;
+                use.address = address;
+                use.password = password;
+                use.role_Id = 1;
+
+                ActivityhubEntities2 fe = new ActivityhubEntities2();
+                fe.user_table.Add(use);
+                fe.SaveChanges();
+
+                return RedirectToAction("ProfilePage");
+
+            }
+
+
+            return RedirectToAction("LoginFailed");
+        }
+
+
+
+        public ActionResult LoginAndRedirect(FormCollection gd)
+        {
+            string email = gd["email"];
+            string password = gd["password"];
+
+            using (var dbContext = new ActivityhubEntities2())
+            {
+                var user = dbContext.user_table.FirstOrDefault(a => a.email == email && a.password == password);
+
+                if (user != null)
+                {
+                    return RedirectToAction("ProfilePage");
+                }
+            }
+
+            return RedirectToAction("LoginFailed");
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
 
 
 
